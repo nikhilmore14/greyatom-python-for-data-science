031364f434b0a1792651b74a7666ca859194d7bc
nikhilmore14 <nikhilmore14@users.noreply.github.com>
2020-09-27 18:52:06 +0530

    Code file created.

---
+++ Data-Visualization-on-Loan-dataset/code.py
@@ -0,0 +1,74 @@
+# --------------
+#Importing header files
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+data=pd.read_csv(path)
+
+loan_status=data['Loan_Status'].value_counts(ascending=False)
+loan_status.plot(kind='bar')
+plt.show()
+#Code starts here
+
+
+# --------------
+#Code starts here
+property_and_loan=data.groupby(['Property_Area','Loan_Status']).size().unstack()
+
+# Plot stacked bar chart
+property_and_loan.plot(kind='bar',stacked=False,figsize=(15,10))
+plt.xlabel('Property Area',rotation=45)
+plt.ylabel('Loan Status')
+plt.show()
+
+
+# --------------
+#Code starts here
+education_and_loan=data.groupby(['Education','Loan_Status']).size().unstack()
+education_and_loan.plot(kind='bar',stacked=True,figsize=(15,10))
+plt.xlabel('Education Status',rotation=45)
+plt.ylabel('Loan Status')
+
+# Plot stacked bar chart
+
+plt.show()
+
+
+# --------------
+#Code starts here
+graduate=data[data['Education']=='Graduate']
+not_graduate=data[data['Education']=='Not Graduate']
+graduate['LoanAmount'].plot(kind='density', label='Graduate')
+
+not_graduate['LoanAmount'].plot(kind='density', label='Not Graduate')
+
+
+
+
+
+
+
+
+
+#Code ends here
+
+#For automatic legend display
+plt.legend()
+
+
+# --------------
+#Code starts here
+fig, (ax_1,ax_2,ax_3) = plt.subplots(nrows = 3 , ncols = 1)
+
+ax_1.scatter(data['ApplicantIncome'],data['LoanAmount'])
+ax_1.set_title('Income')
+ax_2.scatter(data['CoapplicantIncome'],data['LoanAmount'])
+ax_2.set_title('Coapplicant Income')
+data['TotalIncome']=data['ApplicantIncome']+data['CoapplicantIncome']
+ax_3.scatter(data['TotalIncome'],data['LoanAmount'])
+ax_3.set_title('Total Income')
+
+
+
+
